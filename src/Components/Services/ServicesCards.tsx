93f0316ed764b3b320b5a4e406947400fdf43fb2
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2024-12-29 23:00:53 +0200

    just fixes

--- src/Components/Services/ServicesCards.tsx
+++ src/Components/Services/ServicesCards.tsx
@@ -11,7 +11,7 @@ import {
   TbWorldCode,
 } from "react-icons/tb";
 
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export default function ServicesCards() {
   return (
@@ -25,19 +25,22 @@ export default function ServicesCards() {
           key={category.id}
           className="services-card hover:scale-110"
         >
-          {category.name === "Web Development" ? (
-            <TbWorldCode className="text-4xl text-yellow-500 " />
-          ) : category.name === "Social Media Design" ? (
-            <TbMessage2Heart className="text-4xl text-yellow-500" />
-          ) : category.name === "Graphic Design" ? (
-            <TbVectorBezier className="text-4xl text-yellow-500" />
-          ) : category.name === "UI/UX Design" ? (
-            <TbDeviceMobileCheck className="text-4xl text-yellow-500" />
-          ) : category.name === "Photoshop" ? (
-            <TbBrandAdobe className="text-4xl text-yellow-500" />
-          ) : category.name === "Logo Design" ? (
-            <TbPencilBolt className="text-4xl text-yellow-500" />
-          ) : null}
+          <div className="grid gap-6">
+            {category.name === "Web Development" ? (
+              <TbWorldCode className="text-4xl text-yellow-500 " />
+            ) : category.name === "Social Media Design" ? (
+              <TbMessage2Heart className="text-4xl text-yellow-500" />
+            ) : category.name === "Graphic Design" ? (
+              <TbVectorBezier className="text-4xl text-yellow-500" />
+            ) : category.name === "UI/UX Design" ? (
+              <TbDeviceMobileCheck className="text-4xl text-yellow-500" />
+            ) : category.name === "Photoshop" ? (
+              <TbBrandAdobe className="text-4xl text-yellow-500" />
+            ) : category.name === "Logo Design" ? (
+              <TbPencilBolt className="text-4xl text-yellow-500" />
+            ) : null}
+            <h3 className="text-xl">{category.name}</h3>
+          </div>
           <p>{category.desc}</p>
         </motion.div>
       ))}
