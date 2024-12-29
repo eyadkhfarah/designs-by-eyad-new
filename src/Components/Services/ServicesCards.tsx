d4abb0a4576c82833202537c316a9a46dffd7b71
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2024-12-29 22:40:55 +0200

    test 2

--- src/Components/Services/ServicesCards.tsx
+++ src/Components/Services/ServicesCards.tsx
@@ -11,17 +11,17 @@ import {
   TbWorldCode,
 } from "react-icons/tb";
 
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 export default function ServicesCards() {
   return (
     <div className="grid md:grid-cols-2 gap-9 mt-8">
       {Categories.map((category, i) => (
-        <motion.div
-          initial={{ opacity: 0, translateY: 20 }}
-          whileInView={{ opacity: 1, translateY: 0 }}
-          viewport={{ once: true }}
-          transition={{ duration: 0.3, delay: i * 0.2 }}
+        <div
+          // initial={{ opacity: 0, translateY: 20 }}
+          // whileInView={{ opacity: 1, translateY: 0 }}
+          // viewport={{ once: true }}
+          // transition={{ duration: 0.3, delay: i * 0.2 }}
           key={category.id}
           // className="services-card hover:scale-110"
         >
@@ -39,7 +39,7 @@ export default function ServicesCards() {
             <TbPencilBolt className="text-4xl text-yellow-500" />
           ) : null}
           <p>{category.desc}</p>
-        </motion.div>
+        </div>
       ))}
     </div>
   );
--- src/Components/Services/ServicesSlider.tsx
+++ src/Components/Services/ServicesSlider.tsx
@@ -30,34 +30,35 @@ export default function ServicesSlider() {
   // }, [])
 
   return (
-    <motion.div ref={silder} className="cursor-grab mt-8 overflow-hidden">
-      <motion.div
-        drag="x"
-        dragConstraints={{ right: 100, left: -300 }}
-        className="translate-x-24 flex gap-9"
-      >
-        {Categories.map((category) => (
-          <div
-            key={category.id}
-            className="services-card flex flex-col justify-center items-center w-72 h-64 pointer-events-none"
-          >
-            {category.name === "Web Development" ? (
-              <TbWorldCode className="text-6xl text-yellow-500" />
-            ) : null || category.name === "Social Media Design" ? (
-              <TbMessage2Heart className="text-6xl text-yellow-500" />
-            ) : null || category.name === "Graphic Design" ? (
-              <TbVectorBezier className="text-6xl text-yellow-500" />
-            ) : null || category.name === "UI/UX Design" ? (
-              <TbDeviceMobileCheck className="text-6xl text-yellow-500" />
-            ) : null || category.name === "Photoshop" ? (
-              <TbBrandAdobe className="text-6xl text-yellow-500" />
-            ) : null || category.name === "Logo Design" ? (
-              <TbPencilBolt className="text-6xl text-yellow-500" />
-            ) : null}
-            <h3 className="text-xl text-center">{category.name}</h3>
-          </div>
-        ))}
-      </motion.div>
-    </motion.div>
+    <></>
+    // <motion.div ref={silder} className="cursor-grab mt-8 overflow-hidden">
+    //   <motion.div
+    //     drag="x"
+    //     dragConstraints={{ right: 100, left: -300 }}
+    //     className="translate-x-24 flex gap-9"
+    //   >
+    //     {Categories.map((category) => (
+    //       <div
+    //         key={category.id}
+    //         className="services-card flex flex-col justify-center items-center w-72 h-64 pointer-events-none"
+    //       >
+    //         {category.name === "Web Development" ? (
+    //           <TbWorldCode className="text-6xl text-yellow-500" />
+    //         ) : null || category.name === "Social Media Design" ? (
+    //           <TbMessage2Heart className="text-6xl text-yellow-500" />
+    //         ) : null || category.name === "Graphic Design" ? (
+    //           <TbVectorBezier className="text-6xl text-yellow-500" />
+    //         ) : null || category.name === "UI/UX Design" ? (
+    //           <TbDeviceMobileCheck className="text-6xl text-yellow-500" />
+    //         ) : null || category.name === "Photoshop" ? (
+    //           <TbBrandAdobe className="text-6xl text-yellow-500" />
+    //         ) : null || category.name === "Logo Design" ? (
+    //           <TbPencilBolt className="text-6xl text-yellow-500" />
+    //         ) : null}
+    //         <h3 className="text-xl text-center">{category.name}</h3>
+    //       </div>
+    //     ))}
+    //   </motion.div>
+    // </motion.div>
   );
 }
